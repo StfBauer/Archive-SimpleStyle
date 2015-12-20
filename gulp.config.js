1640d2fdb2438ab938b07e0beed1b31f1594e675
Stefan Bauer <stefan.bauer@n8d.at>
2015-12-20 21:05:07 +0100

    App configuration added

--- gulp.config.js
+++ gulp.config.js
@@ -1,15 +1,16 @@
 module.exports = function() {
 
-    var options = {
-        basepath: './app/',
-        baseconfig: {
-            basepath: global.basepath,
-            config: global.basepath + '_config/pattern.conf.json',
-            pattern: global.basepath + '_pattern/',
-            docs: global.basepath + '_pattern.docs/'
-        }
+	// base path of app
+    var basepath = './app/';
+
+    // additional shortcuts
+    var pathConfigs = {
+            basepath: basepath,
+            config: basepath + '_config/pattern.conf.json',
+            pattern: basepath + '_pattern/',
+            docs: basepath + '_pattern.docs/'
     };
 
-    return options;
+    return pathConfigs;
 
 }();
--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,19 @@
 var test = 'hello world';
 
 var gulp = require('gulp'),
-    config = require('./gulp.config')('Hello world'),
+    // base application configuration
+    config = require('./gulp.config')
+    // custom plugins for simple style guide
     ssgCoreConfig = require('./ssg-core/lib/gen-config'),
     ssgCoreCompile = require('./ssg-core/lib/comp-pattern');
 
-gulp.task('debug', function() {
+gulp.task('debug', function(options) {
 
-    console.log(config);
+	console.log(config);
 
 });
 
 // task generate file index
-gulp.task('config', ssgCoreConfig);
+gulp.task('genconfig', ssgCoreConfig);
 
 // compile handlebar patterns
